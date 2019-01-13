ec0a8e6fd99e28445efb0495db32fb5bddac1b05
sussica <sussicaya@gmail.com>
2019-01-13 18:02:19 -0500

    Refactor index.js to use express.js

--- index.js
+++ index.js
@@ -1,36 +1,32 @@
-var http = require('http');
-var url = require('url');
-var fs = require('fs');
+const express = require('express');
+const app = express();
+const port = 1106;
+var path = require('path');
 
-
-//create a server object:
-http.createServer(function (req, res) {
-    var q = url.parse(req.url, true);
-
-    // to process the request with position information for clients
-    if(q.pathname =='/position'){
-        //process the request restaurants and send the suggestion back
-        res.end();
-    
-        //else handling sending back resources to clients
-    } else {
-
-        //to access the home.html just by '/'
-        if (q.pathname == '/') {
-            req.url = '/home.html';
-        }
-
-        fs.readFile(req.url.substring(1), function (err, data) {
-            if (err) {
-                res.writeHead(404, { 'Content-Type': 'text/html' });
-                return res.end("404 Not Found");
+// Call back function for handling the first request
+// the first request from the client: load the page home.html, as well as
+// handle things for main.css and main.js
+// to access the home.html just by '/'
+app.route(['/main.css', '/main.js', '/home.html', '/'])
+        .get((req, res) => {
+            if (req.url == '/') {
+                req.url = '/home.html';
             }
-            res.writeHead(200, { 'Content-Type': 'text/html' });
-            res.write(data);
-            res.end(); //end the response
-        });       
-    }
-}).listen(8080); //the server object listens on port 8080
+            res.sendFile(path.join(__dirname + req.url));
+        });
+
+        
+
+
+
+        
+app.route('/')
+    .post(function (req, res) {
+        /// to process the request with position information for clients
+    })
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
 
 
 

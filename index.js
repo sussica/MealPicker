ca0f7b768cd5ea10dd00c609fd4e50a1ab84d40c
sussica <sussicaya@gmail.com>
2019-01-19 16:09:40 -0500

    Calling google places API and presenting a randomly restaurant

--- index.js
+++ index.js
@@ -2,6 +2,12 @@ const express = require('express');
 const app = express();
 const port = 1106;
 var path = require('path');
+var request = require('request');
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true }));
+
 
 // Call back function for handling the first request
 // the first request from the client: load the page home.html, as well as
@@ -15,10 +21,29 @@ app.route(['/main.css', '/main.js', '/home.html', '/'])
             res.sendFile(path.join(__dirname + req.url));
         });
 
-
 app.route('/')
     .post(function (req, res) {
-        /// to process the request with position information for clients
+        var requestJSON = req.body;
+        console.log(req);
+        var lat = requestJSON.latitude;
+        var lon = requestJSON.longitude;
+        var radius = 1000;
+        var APIkey = "AIzaSyAaQDVhgRZ-jKBkqQBBpGemUDfdrSrkxrs";
+        var keyword = "food";
+        var URL = "https://maps.googleapis.com/maps/api/place/nearbysearch/"+
+        "json?key=" + APIkey + "&radius=" + radius + "&location=" + lat + "," + lon + "&keyword=" + keyword;
+        
+        request(URL, function (error, response, body) {
+            if (!error && response.statusCode == 200) {
+
+                var results = JSON.parse(response.body).results;
+                var count = results.length;
+                var pick = Math.floor(Math.random() * count);               
+
+                res.json(results[pick]);
+            }
+        })
+
     })
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
--- main.js
+++ main.js
@@ -1,30 +1,29 @@
 // helper function for updatePosition because that sorry little ass cant do shit by itself
 function updatePositionHelper(successCallback, errorCallback) {
-    console.log(8);
     successCallback = successCallback || function () {
-        console.log('4')
+        console.log("empty");
     };
     errorCallback = errorCallback || function () {
+        console.log("empty1");
     };
 
     // Try HTML5-spec geolocation.
     var geolocation = navigator.geolocation;
 
-
     if (geolocation) {
-        console.log(9)
+
         // We have a real geolocation service.
         try {
             function handleSuccess(position) {
                 console.log(7);
                 successCallback(position.coords);  // YAAYYYYY!!
             }
-
+          
             geolocation.watchPosition(handleSuccess, errorCallback, {
                 enableHighAccuracy: true, // high accuracy because idk how but this fixes its problems with mobiles
                 maximumAge: 0  // 0 sec. ask for new object everytime user moves.
             });
-            console.log(10)
+
         } catch (err) {
             errorCallback();  // NOOOOOO ;_;
         }
@@ -33,33 +32,53 @@ function updatePositionHelper(successCallback, errorCallback) {
     }
 };
 
-$(document).ready(function(){
+
+function handleGetPlaceSuccess(placeInformation){
+    var placeJSON = placeInformation;
+    console.log(placeJSON);
+    $("#paraRestaurantName").html("hello: "+placeJSON);
+};
+
+
+$(document).ready(function(){   
     var lat;
     var lon;
-    $("#btnSendPosition").click(function(){
-        //TODO: send position to server
 
-        if(lat === undefined || lon === undefined){
-            $("#errorPara").css("visibility", "visible");
-        }else{
-        $("#paraRestaurantName").html("restaurant name latitude: " + lat + ", longtitude: "+ lon);}
-    });
-    
-    updatePositionHelper( 
+    console.log(10);
+
+    updatePositionHelper(
         (coords) => {
-            console.log(`3`);
             lat = coords.latitude;
             lon = coords.longitude;
-            console.log(lat + ", " + lon);
+            console.log(coords);
             $("#errorPara").css("visibility", "hidden");
-        }, 
+        },
         () => {
-            console.log(`2`);
-            $("#errorPara").css("visibility", "visible");}
-        )
+            console.log("error");
+            $("#errorPara").css("visibility", "visible");
+        }
+    );
+
+    console.log(2);
+
+    $("#btnSendPosition").click(function(){
+        if(lat === undefined || lon === undefined){
+            console.log([lat, lon]);
+            $("#errorPara").css("visibility", "visible");
+            lat = 43.7796416;
+            lon = -79.415927;
+        }
+            $.ajax({
+                type: "POST",
+                url: "/",
+                data: { "latitude": lat, "longitude": lon },
+                success: handleGetPlaceSuccess
+            });
+    });
+    
+
     }
 );
 
 
-//TODO: print latitude and longtitude in the paragraph on button click;
 
